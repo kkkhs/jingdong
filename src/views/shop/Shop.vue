f503107f2ae4d32967b2329e5604ecf43af4035e
khs <khs@123.com>
2024-01-01 22:42:38 +0800

    10.5-商家页面搜索布局及路由跳转

--- src/views/shop/Shop.vue
+++ src/views/shop/Shop.vue
@@ -1,26 +1,46 @@
 <template>
   <div class="wrapper">
-    <ShopInfo :item="item" :hideBorder="true"/>
+    <div class="search">
+      <div
+        class="search__back iconfont"
+        @click="handleBackClick"
+      >
+        &#xe608;
+      </div>
+      <div class="search__content">
+        <span class="search__content__icon iconfont">&#xecc4;</span>
+        <input class="search__content__input" placeholder="请输入商品名称"/>
+      </div>
+    </div>
+    <ShopInfo :item="data.item" :hideBorder="true"/>
   </div>
 </template>
 
 <script>
 import ShopInfo from '@/components/ShopInfo'
+import { get } from '@/utils/request'
+import { reactive } from 'vue'
+import { useRouter } from 'vue-router'
 export default ({
   name: 'Shop',
   components: { ShopInfo },
   setup() {
-    const item = {
-      _id: '1',
-      name: '沃尔玛',
-      imgUrl: 'http://www.dell-lee.com/imgs/vue3/near.png',
-      sales: 10000,
-      expressLimit: 0,
-      expressPrice: 5,
-      slogan: 'VIP尊享满89元减4元运费券'
+    const router = useRouter()
+    const data = reactive({ item: {} })
+    const getItemData = async () => {
+      const result = await get('/api/shop/1')
+      if (result?.errno === 0 && result?.data) {
+        data.item = result.data
+      }
     }
 
-    return { item }
+    getItemData()
+
+    const handleBackClick = () => {
+      router.push({ name: 'Home' })
+    }
+
+    return { data, handleBackClick }
   }
 })
 </script>
@@ -28,5 +48,40 @@ export default ({
 <style lang="scss" scoped>
 .wrapper{
   padding: 0 .18rem;
+}
+.search{
+  display: flex;
+  margin: .14rem 0 .04rem 0;
+  line-height: .32rem;
+  &__back{
+    width: .3rem;
+    font-size: .24rem;
+    color: #b6b6b6;
+  }
+  &__content{
+    display: flex;
+    flex: 1;
+    background: #f5f5f5;
+    border-radius: .16rem;
+    &__icon{
+      width: .44rem;
+      text-align: center;
+      color: #b7b7b7;
+    }
+    &__input{
+      padding-right: .2rem;
+      display: block;
+      width: 100%;
+      border: none;
+      outline: none;
+      background: none;
+      height: .32rem;
+      font-size: .14rem;
+      color: #333;
+      &::placeholder{
+        color: #333;
+      }
+    }
+  }
 }
 </style>
